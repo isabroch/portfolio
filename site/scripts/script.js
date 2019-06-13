6e01aec099f816c160ef01d0d5915be9bcc0a689
Isabella Broch <isab7099@next-rebb110-19.local>
2019-06-13 15:36:43 +0200

    Project page arrow functions
    - Starting project array list and using search values to dictate which project is shown

--- site/scripts/script.js
+++ site/scripts/script.js
@@ -1,77 +1,105 @@
 // For typing effect on title
-var typed = new Typed('#typed', {
-  stringsElement: '#typed-strings',
-  typeSpeed: 30,
-  backSpeed: 40,
-  backDelay: 500,
-});
-
-
-// Having issues???
-// // Isotope sorting
-// var $isoGrid = $('.work--projects').isotope({
-//   // options
-//   itemSelector: '.work--project',
-//   layoutMode: 'fitRows'
-// });
-
-// const sortButtons = document.querySelectorAll('.filter--button');
-// sortButtons.forEach(function (button) {
-//     button.addEventListener('click', function() {
-//       let filterValue = this.dataset.filter;
-//       console.log(filterValue);
-//     $isoGrid.isotope({
-//       filter: filterValue
-//     })
-//   });
-// });
-
-
-// For skills bar animation
-const skillsArray = document.querySelectorAll('.skill');
-const skillWrapper = document.querySelector('.skill--wrapper');
-
-function skillAnimation() {
-  skillsArray.forEach(function (skill, i) {
-    let skillLevel = skill.querySelector('.skill--bar').dataset.lvl;
-    let skillBar = skill.querySelector('.skill--level')
-
-    setTimeout(() => {
-      skillBar.style.width = skillLevel;
-    }, 300 * i);
+if (document.querySelector('#typed-strings')) {
+  var typed = new Typed('#typed', {
+    stringsElement: '#typed-strings',
+    typeSpeed: 30,
+    backSpeed: 40,
+    backDelay: 500,
   });
 }
 
-// If skillWrapper is 50px in vp from bottom, then run the function
-let observer = new IntersectionObserver((entries, observer) => {
-  entries.forEach(function (entry) {
-    if (entry.isIntersecting) {
-      skillAnimation();
-      observer.unobserve;
-    }
-  })
-}, {
-  root: null,
-  rootMargin: "0px 0px -50px 0px"
-});
 
-observer.observe(skillWrapper);
+let index = false;
 
-document.querySelectorAll('.readmore--button').forEach((button) => {
-  button.addEventListener('click', () => {
-    const buttonText = button.querySelector('.readmore--button--text--wrapper');
+if (index) {
+  // Having issues???
+  // // Isotope sorting
+  // var $isoGrid = $('.work--projects').isotope({
+  //   // options
+  //   itemSelector: '.work--project',
+  //   layoutMode: 'fitRows'
+  // });
 
-    console.log(buttonText);
+  // const sortButtons = document.querySelectorAll('.filter--button');
+  // sortButtons.forEach(function (button) {
+  //     button.addEventListener('click', function() {
+  //       let filterValue = this.dataset.filter;
+  //       console.log(filterValue);
+  //     $isoGrid.isotope({
+  //       filter: filterValue
+  //     })
+  //   });
+  // });
 
-    if (button.classList.contains('om-mig--readmore')) {
-      const content = document.querySelector('.readmore--text');
-      content.classList.toggle('hiding');
 
-      if (content.classList.contains('hiding')) {
-        buttonText.textContent = 'Read more';
-      } else {
-        buttonText.textContent = 'Read less';
+  // For skills bar animation
+  const skillsArray = document.querySelectorAll('.skill');
+  const skillWrapper = document.querySelector('.skill--wrapper');
+
+  function skillAnimation() {
+    skillsArray.forEach(function (skill, i) {
+      let skillLevel = skill.querySelector('.skill--bar').dataset.lvl;
+      let skillBar = skill.querySelector('.skill--level')
+
+      setTimeout(() => {
+        skillBar.style.width = skillLevel;
+      }, 300 * i);
+    });
+  }
+
+  // If skillWrapper is 50px in vp from bottom, then run the function
+  let observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach(function (entry) {
+      if (entry.isIntersecting) {
+        skillAnimation();
+        observer.unobserve;
       }
-    }
+    })
+  }, {
+    root: null,
+    rootMargin: "0px 0px -50px 0px"
+  });
+
+  observer.observe(skillWrapper);
+
+  document.querySelectorAll('.readmore--button').forEach((button) => {
+    button.addEventListener('click', () => {
+      const buttonText = button.querySelector('.readmore--button--text--wrapper');
+
+      console.log(buttonText);
+
+      if (button.classList.contains('om-mig--readmore')) {
+        const content = document.querySelector('.readmore--text');
+        content.classList.toggle('hiding');
+
+        if (content.classList.contains('hiding')) {
+          buttonText.textContent = 'Read more';
+        } else {
+          buttonText.textContent = 'Read less';
+        }
+      }
+    })
   })
-})
+}
+
+
+// For project page, scrolling to side
+const projectArray = [
+  '1950s',
+  'om_verden',
+  'moana',
+  'krigsmuseet',
+  'national_graphic',
+  'rfa_party',
+  'mediegrafiker_exam',
+  'hundlufter'
+]
+
+let projectNumber = window.location.search || 0;
+console.log(projectNumber);
+
+if (projectNumber != 0) {
+
+}
+
+console.log(projectNumber);
