30db6ab094fcee9fdc6291c74300b1ed195d4ece
Isabella Broch <isabrochweb@gmail.com>
2021-06-02 11:07:21 +0200

    update cv

---
+++ .test.js
@@ -0,0 +1,45 @@
+// setting up data structures for information?
+
+const hero = {
+  strings: [  { en: "Graphic Design", da: "Grafisk design" },
+  { en: "Web Development", da: "Web udvikling" },
+  { en: "Concept to Reality", da: "Koncept til virkelighed" },],
+  subtitle: {en: "The digital solution your ideas deserve", da: "Den digitale løsning din ide fortjener"}
+}
+
+const projects = [
+  {
+    name: { en: "English Project 1", dk: "Danish Project 1" },
+    description: { en: "English Description", dk: "Danish Description" },
+    link: "",
+    img: "",
+  },
+
+  {
+    name: { en: "English Project 2", dk: "Danish Project 2" },
+    description: "Generic description for both languages!",
+    link: "",
+    img: "",
+  },
+
+  {
+    name: { dk: "Only Danish name" },
+    description: "Generic description for both languages!",
+    link: "",
+    img: "",
+  },
+
+  {
+    name: "Generic name for both languages!",
+    description: { en: "English Description", dk: "Danish Description" },
+    link: "",
+    img: "",
+  },
+
+  {
+    name: "Generic name for both languages!",
+    description: "Generic description for both languages!",
+    link: "",
+    img: "",
+  },
+];
